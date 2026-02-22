9530a146875adc09244a4f6191d626d9d62fcb5c
Pankaj <pankajmonde96@gmail.com>
2026-02-22 23:08:02 +0530

    Added solution - LeetHub

---
+++ 8279-java-endoffile-Hackerrank/8279-java-endoffile.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int linenumver=1;
+        while(sc.hasNext()){
+            String line=sc.nextLine();
+             System.out.println(linenumver+" "+line);
+             linenumver++;
+               
+          
+        }
+        sc.close();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
