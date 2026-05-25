ae833d37c3087c83ee2f1f1048bf0905e2d7888c
Pankaj <pankajmonde96@gmail.com>
2026-05-25 21:16:41 +0530

    Added solution - LeetHub

---
+++ 9779-java-inheritance-i-Hackerrank/9779-java-inheritance-i.java
@@ -0,0 +1,19 @@
+
+
+class Bird extends Animal {
+
+    void fly() {
+        System.out.println("I am flying");
+    }
+
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
+
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
