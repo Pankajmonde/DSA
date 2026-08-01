975a40bc4393359b0932bb9b0c56acd4b9fe0940
Pankaj <pankajmonde96@gmail.com>
2026-08-01 23:30:35 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 22905-java-1d-array-Hackerrank/22905-java-1d-array.java
@@ -0,0 +1,18 @@
+
+
+ 
+
+int[] a = new int[n];
+
+for (int i = 0; i < n; i++) {
+    a[i] = scan.nextInt();
+}
+
+
+   
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
