b7251d7b0b8d04e32d61344ce950bb73f61f978b
Pankaj <pankajmonde96@gmail.com>
2026-09-03 16:14:36 +0530

    Time: 13 ms (28.32%), Space: 45.9 MB (50.61%) - LeetHub - Time: O(n), Space: O(n)

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,40 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-           if (s.length() != t.length()) {
-            return false;
+        if(s.length()!=t.length()){
+        return false; 
         }
-           int []count = new int[26];
-           for(int i=0; i<s.length(); i++){
-            count[s.charAt(i)- 'a']++;
-            count[t.charAt(i)- 'a']--;
-           }
-           for(int value : count ){
-            if(value !=0){
-                return false;
-            }
-           }
-           return true;
-    }
+        HashMap<Character, Integer>map1 = new HashMap<>();
+        HashMap<Character, Integer>map2 = new HashMap<>();
+
+
+        for(char ch: s.toCharArray()){
+            map1.put(ch, map1.getOrDefault(ch, 0)+1);
+        }
+         for(char ch: t.toCharArray()){
+            map2.put(ch, map2.getOrDefault(ch, 0)+1);
+        }
+
+         
+        
+        return map1.equals(map2);
+        
+          //        if (s.length() != t.length()) {
+    //         return false;
+    //     }
+    //        int []count = new int[26];
+    //        for(int i=0; i<s.length(); i++){
+    //         count[s.charAt(i)- 'a']++;
+    //         count[t.charAt(i)- 'a']--;
+    //        }
+    //        for(int value : count ){
+    //         if(value !=0){
+    //             return false;
+    //         }
+    //        }
+    //        return true;
+    // }
+
+}
 
 }
 
